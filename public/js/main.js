a7b787af219a77dfda8a7c928357b3d995acafd9
= <=>
2018-10-18 19:32:15 +0500

    Final Commit

---
+++ public/js/main.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+  let messages = [];
+  let socket = io.connect("http://localhost:3000");
+  let chatForm = $("#chatForm");
+  let message = $("#chatInput");
+  let chatWindow = $("#chatWindow");
+  let userForm = $("#userForm");
+  let username = $("#username");
+  let users = $("#users");
+  let error = $("#error");
+
+  /// Submit user form
+  userForm.on("submit", function(e) {
+    e.preventDefault();
+    socket.emit("set user", username.val(), function(data) {
+      if (data) {
+        $("#userFormWrap").hide();
+        $("#mainWrap").show();
+      } else {
+        error.html("Username is taken");
+      }
+    });
+  });
+
+  /// Submit Chat Form
+  chatForm.on("submit", function(e) {
+    socket.emit("send message", message.val());
+    message.val("");
+    e.preventDefault();
+  });
+
+  socket.on("show message", function(data) {
+    chatWindow.append(
+      "<strong>" + data.user + "</strong>: " + data.msg + "<br/>"
+    );
+  });
+
+  /// Display username
+  socket.on("users", function(data) {
+    let html = "";
+    for (let i = 0; i < data.length; i++) {
+      html += "<li class='list-group-item'>" + data[i] + "</li>";
+    }
+    users.html(html);
+  });
+});
